9b11babd3d301636dabd50016cfd45a217d70b38
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2015-10-04 13:26:00 +0100

    queue implementaion in js

---
+++ queue.js
@@ -0,0 +1,123 @@
+// Implementations of queues
+
+// LINKED LIST IMPLEMENTATION (modified from http://www.thatjsdude.com/interview/linkedList.html)
+
+//Singly Linked List
+
+//one pointer (head) to point the first node of your linked list
+function LinkedList(){
+  this.head = null;
+}
+
+// push to the list
+
+LinkedList.prototype.push = function(val){
+    // the new node
+    var node = {
+       value: val,
+       next: null
+    }
+    // if head node is null, add the new node to head
+    if(!this.head){
+      this.head = node;
+    }
+    // else traverse through the linked list until encounter node with next:null
+    else{
+      var current = this.head;
+      while(current.next){
+        current = current.next;
+      }
+      // put the new node at the end of the list
+      current.next = node;
+    }
+}
+
+// pop from the list
+
+LinkedList.prototype.shift = function(){
+
+    // if head node is null, the stack is empty
+    if(!this.head){
+      console.log('stack is empty');
+      return;
+    }
+     else {
+        var current = this.head;
+        this.head = current.next;
+        console.log('removing a node')
+        console.log(current);
+        current = null;
+    }
+}
+
+//Doubly Linked List
+
+function DoublyLinkedList(){
+   this.head = null;
+}
+
+// push to the list
+
+DoublyLinkedList.prototype.push = function(val){
+    // the new node
+    var node = {
+       value: val,
+       next: null,
+       previous: null
+    }
+
+    // if head node is null, add the new node to head
+    if(!this.head){
+      this.head = node;
+    }
+    // else traverse through the linked list until encounter node with next:null
+    else{
+        var current = this.head;
+        var previous = this.head;
+        while (current.next){
+            previous = current;
+            current = current.next;
+        }
+        // put the new node at the end of the list
+        current.next = node;
+        // reference the previous node in the new node
+        current.next.previous = current;
+    }
+
+}
+
+// pop from the list
+
+DoublyLinkedList.prototype.shift = function(){
+
+    // if head node is null, the stack is empty
+    if(!this.head){
+      console.log('stack is empty');
+      return;
+    }
+    // else traverse through the linked list until encounter node with next:null
+    else {
+        var current = this.head;
+        this.head = current.next;
+        console.log('removing a node')
+        console.log(current);
+        current = null;
+    }
+}
+
+// ARRAY IMPLEMENTATION
+
+// create stack
+var stack = [];
+
+// push to stack
+stack.push(1);
+stack.push(2);
+
+//shift from stack
+stack.shift();
+stack.shift();
+
+
+
+
